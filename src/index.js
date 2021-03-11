401731390329ce8ac020a9b94b1b63fdcf942d77
Roman <roman.panchenko@dstarlab.com>
2021-03-11 16:07:17 +0200

    init: task in process

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+    let arrOfSemi = bracketsConfig.map((i) => [`^\\${i[0]}.*\\${i[1]}$`]);
+    let arrOfRegex = arrOfSemi.map(i => new RegExp(arrOfSemi[0], 'g'));
+    return arrOfRegex.every(el => el.test(str));
+
 }
+
+
+
+
+
