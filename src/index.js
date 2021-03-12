6807f07293291d98c81800142ae2cec46ee3075f
Roman <roman.panchenko@dstarlab.com>
2021-03-12 17:10:33 +0200

    feat: additional condition

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ module.exports = function check(str, bracketsConfig) {
         return false;
     }
     let arrOfSemi = bracketsConfig.map((i) => [`^\\${i[0]}.*\\${i[1]}$`]);
+    let withoutSemi = bracketsConfig.map((i) => [`\\${i[0]}.*\\${i[1]}`]);
+    let ecxeptionsSemi = bracketsConfig.map((i) => [`\\${i[0]}.{1}\\${i[1]}`]);
+    let regexWithout = withoutSemi.map(i => new RegExp(withoutSemi[0], 'g'));
     let arrOfRegex = arrOfSemi.map(i => new RegExp(arrOfSemi[0], 'g'));
-    return arrOfRegex.every(el => el.test(str));
+    let ecxeptionsRegex = ecxeptionsSemi.map(i => new RegExp(ecxeptionsSemi[0], 'g'))
+
+    if (!ecxeptionsRegex.some(el => el.test(str))) {
+        return true
+    }
+    else if (arrOfRegex.every(el => el.test(str))) {
+        return true
+    }
+    return false
+
 
 }
 
-
-
+// let result = check('())(',  [['(', ')']])
+// console.log(result)
 
 
