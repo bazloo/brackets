44f3fd23153713419f33cdf14c47b70a62124aea
Roman Panchenko <timberspirit@gmail.com>
2021-03-15 08:36:00 +0200

    feat: update

--- src/index.js
+++ src/index.js
@@ -1,26 +1,34 @@
-module.exports = function check(str, bracketsConfig) {
-    if (str.length % 2 !== 0) {
-        return false;
+//module.exports =
+    function check(str, bracketsConfig) {
+    let newStr = str;
+    let preparedForRegex =
+    (typeof parseInt(bracketsConfig[0][0]) === 'number')
+    ? bracketsConfig.map((i) => i[0] + i[1])
+    : bracketsConfig.map((i) => `\\${i[0]}\\${i[1]}`)
+    let possibleAmount = (str.length / 2) + 2;
+    let arrOfRegex = preparedForRegex.map((i) => new RegExp(i, 'g'));
+        while (possibleAmount > 0) {
+
+            for(let i = 0; i < arrOfRegex.length; i++){
+                        if(i === arrOfRegex.length -1 ){possibleAmount--}
+                        newStr = newStr.replace(arrOfRegex[i], '');
+
+
+                    }
+        }
+        // if (arrOfRegex.some(el => el.test(newStr))) {
+        //     for(let i = 0; i < arrOfRegex.length; i++){
+        //         newStr = newStr.replace(arrOfRegex[i], '')
+        //     }
+        //     return check(newStr, bracketsConfig)
+        // }
+        if(newStr === ''){
+            return true
+        }
+        return false
     }
-    let arrOfSemi = bracketsConfig.map((i) => [`^\\${i[0]}.*\\${i[1]}$`]);
-    let withoutSemi = bracketsConfig.map((i) => [`\\${i[0]}.*\\${i[1]}`]);
-    let ecxeptionsSemi = bracketsConfig.map((i) => [`\\${i[0]}.{1}\\${i[1]}`]);
-    let regexWithout = withoutSemi.map(i => new RegExp(withoutSemi[0], 'g'));
-    let arrOfRegex = arrOfSemi.map(i => new RegExp(arrOfSemi[0], 'g'));
-    let ecxeptionsRegex = ecxeptionsSemi.map(i => new RegExp(ecxeptionsSemi[0], 'g'))
 
-    if (!ecxeptionsRegex.some(el => el.test(str))) {
-        return true
-    }
-    else if (arrOfRegex.every(el => el.test(str))) {
-        return true
-    }
-    return false
-
-
-}
-
-// let result = check('())(',  [['(', ')']])
-// console.log(result)
+let res = check('([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])((([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])))', [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']])
+console.log(res)
 
 
