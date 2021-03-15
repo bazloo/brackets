c9d47a7a4ffb88de1057ac3294feed84cb2807b2
Roman <roman.panchenko@dstarlab.com>
2021-03-15 12:12:31 +0200

    fix: finally understood what should be done in this task.

--- src/index.js
+++ src/index.js
@@ -1,34 +1,24 @@
-//module.exports =
-    function check(str, bracketsConfig) {
+module.exports = function check(str, bracketsConfig) {
     let newStr = str;
-    let preparedForRegex =
-    (typeof parseInt(bracketsConfig[0][0]) === 'number')
-    ? bracketsConfig.map((i) => i[0] + i[1])
-    : bracketsConfig.map((i) => `\\${i[0]}\\${i[1]}`)
-    let possibleAmount = (str.length / 2) + 2;
-    let arrOfRegex = preparedForRegex.map((i) => new RegExp(i, 'g'));
-        while (possibleAmount > 0) {
+    let possibleAmountOfClosedBrackets = str.length / 2;
+    let arrOfClosedBrackets = bracketsConfig.map(el => el[0] + el[1]);
 
-            for(let i = 0; i < arrOfRegex.length; i++){
-                        if(i === arrOfRegex.length -1 ){possibleAmount--}
-                        newStr = newStr.replace(arrOfRegex[i], '');
-
-
-                    }
+    while (possibleAmountOfClosedBrackets > 0) {
+        for (let i = 0; i < arrOfClosedBrackets.length; i++) {
+            let pairOfBracketsToReplace = arrOfClosedBrackets[i];
+            if (i === arrOfClosedBrackets.length - 1) {
+                possibleAmountOfClosedBrackets--
+            }
+            newStr = newStr.replace(pairOfBracketsToReplace, '');
         }
-        // if (arrOfRegex.some(el => el.test(newStr))) {
-        //     for(let i = 0; i < arrOfRegex.length; i++){
-        //         newStr = newStr.replace(arrOfRegex[i], '')
-        //     }
-        //     return check(newStr, bracketsConfig)
-        // }
-        if(newStr === ''){
-            return true
-        }
-        return false
     }
 
-let res = check('([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])((([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])))', [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']])
-console.log(res)
+    if (newStr === '') {
+        return true;
+    }
+    return false;
+}
+
+
 
 
